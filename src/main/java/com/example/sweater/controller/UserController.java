6e449685b72fa738d22ba0cbbf59ea3030928f82
Denis <dzyanis92@gmail.com>
2025-01-10 19:53:59 +0300

    Sweater

--- src/main/java/com/example/sweater/controller/UserController.java
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -61,4 +61,41 @@ userService.saveUser(user, username, form);
 
         return "redirect:/user/profile";
     }
+    @GetMapping("subscribe/{user}")
+    public String subscribe(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user
+    ) {
+        userService.subscribe(currentUser, user);
+
+        return "redirect:/user-messages/" + user.getId();
+    }
+
+    @GetMapping("unsubscribe/{user}")
+    public String unsubscribe(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user
+    ) {
+        userService.unsubscribe(currentUser, user);
+
+        return "redirect:/user-messages/" + user.getId();
+    }
+
+    @GetMapping("{type}/{user}/list")//подписчики или подписки
+    public String userList(
+            Model model,
+            @PathVariable User user,
+            @PathVariable String type
+    ) {
+        model.addAttribute("userChannel", user);
+        model.addAttribute("type", type);
+
+        if ("subscriptions".equals(type)) {
+            model.addAttribute("users", user.getSubscriptions());
+        } else {
+            model.addAttribute("users", user.getSubscribers());
+        }
+
+        return "subscriptions";
+    }
 }
--- src/main/resources/templates/profile.ftl
+++ src/main/resources/templates/profile.ftl
@@ -1,28 +1,21 @@
 <#import "parts/common.ftl" as c>
-
-
 <@c.page>
 <h5>${username}</h5>
 ${message?ifExists}
 <form method="post">
-
     <div class="form-group row">
         <label class="col-sm-2 col-form-label">Пароль:</label>
         <div class="col-sm-4">
             <input type="password" name="password" class="form-control" placeholder="Пароль" />
         </div>
     </div>
-
     <div class="form-group row">
             <label class="col-sm-2 col-form-label">E-mail:</label>
             <div class="col-sm-4">
                 <input type="email" name="email" class="form-control" placeholder="name@mail.ru" value="${email!''}" />
             </div>
         </div>
-
-
     <input type="hidden" name="_csrf" value="${_csrf.token}" />
-
     <button class="btn btn-primary" type="submit">Сохранить</button>
 </form>
 </@c.page>
