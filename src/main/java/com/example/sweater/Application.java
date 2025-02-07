74e964247f97203b580bbd8f854ec51a9064e883
Sinichenko Denis <dzyanis92@gmail.com>
2025-02-07 19:03:02 +0300

    удалил лишнее

--- src/main/java/com/example/sweater/Application.java
+++ src/main/java/com/example/sweater/Application.java
@@ -1,17 +1,16 @@
 package com.example.sweater;
 
 import com.example.sweater.service.PasswordService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"org.example.mypassword", "com.example.sweater"})
 @SpringBootApplication
 public class Application {
 
     @Autowired
     private PasswordService passwordService;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
