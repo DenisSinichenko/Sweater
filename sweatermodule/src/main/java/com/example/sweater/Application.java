95082ece7d177cf9ccc68333bfed6436e68b0ece
Sinichenko Denis <dzyanis92@gmail.com>
2025-02-14 15:42:12 +0300

    добавил модульность

---
+++ sweatermodule/src/main/java/com/example/sweater/Application.java
@@ -0,0 +1,17 @@
+package com.example.sweater;
+
+import com.example.sweater.service.PasswordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    @Autowired
+    private PasswordService passwordService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ sweatermodule/src/main/java/com/example/sweater/repos/UserRepo.java
@@ -0,0 +1,16 @@
+package com.example.sweater.repos;
+
+import com.example.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    //todo переделать username на userName
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+
+    User findByEmail(String email);
+
+
+}
