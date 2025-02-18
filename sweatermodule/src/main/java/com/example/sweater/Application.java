4104871443d414e12c6a1b30e147210fe906f47b
Sinichenko Denis <dzyanis92@gmail.com>
2025-02-18 09:29:42 +0300

    исправил import в Application.java

--- sweatermodule/src/main/java/com/example/sweater/Application.java
+++ sweatermodule/src/main/java/com/example/sweater/Application.java
@@ -1,6 +1,7 @@
 package com.example.sweater;
 
 import com.example.sweater.service.PasswordService;
+import com.sweater.encpassword.EncryptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
